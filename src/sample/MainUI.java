6a31c747b6eb22f2e5509b9494a8f8538477f859
MrBasZ <58010819@kmitl.ac.th>
2017-10-25 04:38:57 +0700

    add event mouse move windows

--- src/sample/MainUI.java
+++ src/sample/MainUI.java
@@ -1,19 +1,52 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainUI extends Application {
 
+    Stage stage;
+    //init xy offsets
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        this.stage = primaryStage;
+
         Parent root = FXMLLoader.load(getClass().getResource("settingUI.fxml"));
-        primaryStage.setTitle("SETTING");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        //set mouse pressed
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        //set mouse drag
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
+        Scene scene = new Scene(root);
+
+        scene.setFill(Color.TRANSPARENT);
+        stage.setTitle("SETTING");
+        stage.setScene(scene);
+        stage.show();
     }
 
 
